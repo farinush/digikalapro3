f5d850edda29e856d9d3d4d9acba8a6af0bc31ed
farinush <farinushjohndel@gmail.com>
2025-01-22 10:11:27 +0330

    version-1-1.0

--- public/js/menu/topmenu.js
+++ public/js/menu/topmenu.js
@@ -1,12 +1,13 @@
 const topMenu = async () => {
   let topmenu = "";
   try {
-    let res = await axios.get("http://localhost:3004/topMenu");
-    topmenu = res.data.map((item) => {
-      return `<img src="${item.link}" alt="${item.top_menu}" class="w-[100%] !h-[30px] lg:!h-[60px]" />`;
-    });
+    let data=await fetch("http://localhost:3004/topMenu")
+    let res = await data.json();
+    topmenu+=`<img src="${res.link}" alt="${res.top_menu}" class="w-[100%] !h-[30px] lg:!h-[60px]" />`
+      ;
+    
     console.log("topmenu")
-    document.querySelector(".top_menu .a_top_menu").innerHTML = topmenu.join("");
+    document.querySelector(".top_menu .a_top_menu").innerHTML = topmenu;
   } catch (error) {
     console.log("Error fetching top-menu:", error.message);
   }
