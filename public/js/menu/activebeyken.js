e563fc3fb795188e5b909e3956440882d91f7977
farinush <farinushjohndel@gmail.com>
2025-07-11 11:47:39 +0330

    new3

--- public/js/menu/activebeyken.js
+++ public/js/menu/activebeyken.js
@@ -1,16 +1,17 @@
 export const fetchActivebeyken = async () => {
-  let activebeyken = "";
   try {
     let data = await fetch("https://farinush.github.io/digikalapro3/db.json");
     let res = await data.json();
-    activebeyken += `
-        <div class="child__active-beyken lg:w-[71%] w-[90%]   mx-auto ">
-          <a class="subchild__active-beyken w-[100%] h-[100%] flex flex-row flex-wrap justify-around items-center gap-y-[12px]" href="#">
-            <img src="${res.img1}" alt="" class="w-[660px] h-[100%] rounded-[15px]"/>
-            <img src="${res.img2}" alt="" class="w-[660px] h-[100%] rounded-[15px]"/>
+    console.log(res);
+    let activebeyken = res.activebeyken.map((item) => {
+      return `
+        <div class="child__active-beyken lg:w-[71%] w-[90%]   mx-auto" >
+          <a class="subchild__active-beyken w-[100%] h-[100%] flex flex-row flex-wrap justify-around items-center gap-y-[12px]" href="#" key=${item.id}>
+            <img src="${item.img}" alt="" class="w-[660px] h-[100%] rounded-[15px]"/>
           </a>
         </div>`;
-        document.querySelector(".active-beyken").innerHTML=activebeyken;
+    });
+    document.querySelector(".active-beyken").innerHTML = activebeyken;
   } catch (error) {
     console.log(error.message);
   }
