f8764fe29505c1bbe8d3e9ce94efaee9527cd783
farinush <farinushjohndel@gmail.com>
2025-07-11 12:10:10 +0330

    new4

--- public/js/menu/activebeyken.js
+++ public/js/menu/activebeyken.js
@@ -5,13 +5,10 @@ export const fetchActivebeyken = async () => {
     console.log(res);
     let activebeyken = res.activebeyken.map((item) => {
       return `
-        <div class="child__active-beyken lg:w-[71%] w-[90%]   mx-auto" >
-          <a class="subchild__active-beyken w-[100%] h-[100%] flex flex-row flex-wrap justify-around items-center gap-y-[12px]" href="#" key=${item.id}>
-            <img src="${item.img}" alt="" class="w-[660px] h-[100%] rounded-[15px]"/>
-          </a>
-        </div>`;
+            <img src="${item.img}" alt="" class="w-[660px] h-[100%] rounded-[15px]" data-id=${item.id}/>
+          `;
     });
-    document.querySelector(".active-beyken").innerHTML = activebeyken;
+    document.querySelector(".subchild__active-beyken").innerHTML = activebeyken;
   } catch (error) {
     console.log(error.message);
   }
--- public/js/menu/uniqeestil.js
+++ public/js/menu/uniqeestil.js
@@ -1,18 +1,14 @@
 export const fetchUniqeestil = async () => {
-    let uniqeestil = "";
-    try {
-      let data = await fetch("https://farinush.github.io/digikalapro3/db.json");
-      let res = await data.json();
-      uniqeestil += `
-          <div class="child__uniqe-estil w-[90%] lg:w-[71%]   mx-auto ">
-            <a class="subchild__uniqe-estil w-[100%] h-[100%] flex flex-row flex-wrap justify-around items-center gap-y-[12px]" href="#">
-              <img src="${res.img1}" alt="" class="w-[660px] h-[100%] rounded-[15px]"/>
-              <img src="${res.img2}" alt="" class="w-[660px] h-[100%] rounded-[15px]"/>
-            </a>
-          </div>`;
-          document.querySelector(".uniqe-estil").innerHTML=uniqeestil;
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
-  
+  try {
+    let data = await fetch("https://farinush.github.io/digikalapro3/db.json");
+    let res = await data.json();
+    let uniqeestil = res.uniqeestil.map((item) => {
+      return `
+              <img src="${item.img}" alt="" class="w-[660px] h-[100%] rounded-[15px]"/>
+           `;
+    });
+    document.querySelector(".subchild__uniqe-estil").innerHTML = uniqeestil;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
